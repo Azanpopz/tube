50cc9ff4f7069a1d147f3196d19dbdb98ae25201
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-08 00:27:19 +0530

    Create status.py

---
+++ plugins/status.py
@@ -0,0 +1,45 @@
+import os
+import traceback
+import logging
+
+from pyrogram import Client
+from pyrogram import StopPropagation, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+import config
+from handlers.broadcast import broadcast
+from handlers.check_user import handle_user_status
+from handlers.database import Database
+
+LOG_CHANNEL = config.LOG_CHANNEL
+AUTH_USERS = config.AUTH_USERS
+DB_URL = config.DB_URL
+DB_NAME = config.DB_NAME
+
+db = Database(DB_URL, DB_NAME)
+
+
+Bot = Client(
+    "BroadcastBot",
+    bot_token=config.BOT_TOKEN,
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+)
+
+@Client.on_message(filters.private)
+async def _(bot, cmd):
+    await handle_user_status(bot, cmd)
+
+
+
+
+@Client.on_message(filters.private & filters.command("stats"))
+async def sts(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    await m.reply_text(
+        text=f"**Total Users in Database 📂:** `{await db.total_users_count()}`\n\n**Total Users with Notification Enabled 🔔 :** `{await db.total_notif_users_count()}`",
+        parse_mode="Markdown",
+        quote=True
+    )
