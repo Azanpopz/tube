301b81f395817a582dee4f86b660892f66880a0d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-08 00:50:45 +0530

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -6,7 +6,7 @@ from pyrogram import Client
 from pyrogram import StopPropagation, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
-from config import
+
 
 from plugins.check_user import handle_user_status
 from plugins.database import Database
@@ -15,6 +15,18 @@ LOG_CHANNEL = config.LOG_CHANNEL
 AUTH_USERS = config.AUTH_USERS
 DB_URL = config.DB_URL
 DB_NAME = config.DB_NAME
+
+
+
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+APP_ID = int(os.environ.get("APP_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+
 
 db = Database(DB_URL, DB_NAME)
 
